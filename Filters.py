10811d11042f94deaad0622ca1f327a1458fef42
Angela H <ahoch2@uh.edu>
2017-11-27 18:55:25 -0600

    add directional

--- Filters.py
+++ Filters.py
@@ -1,9 +1,7 @@
 """
 TODO:
 -setters and getters
--update generateMask
 -notch
--directional
 """
 """
 Issues:
@@ -26,6 +24,7 @@ To use functions from this class, create a Filter object with the following valu
 
 import numpy as np
 import cv2
+import math
 
 class Filter:
 
@@ -43,31 +42,30 @@ class Filter:
 		else:
 			filter_func = filter_func + "_band"
 
-		self.filter = getattr(self, filter_func)
+		self.filter = getattr(self, filter_func, self.ideal_circle)
 
 	def generateMask(self):
-		"""
-		height, width = self.shape
-		image = np.zeros(shape, dtype=np.uint8)
-		cx, cy = width / 2, height / 2
-		for y in range(height):
-			for x in range(width):
-				r, theta = to_polar(x - cx, y - cy)
-				# image[y,x] = 255 * self.filter_func.evaluate(r, theta)
-				# image[y,x] = 255 * gaussian(r-freq, freq_range/2)
-				# image[y,x] = 255 * butterworth((r-freq)/(freq_range/2), 4)
-				# image[y,x] = 255 * (ideal(r-freq, freq_range/2) - ideal(r-freq, -freq_range/2))
-		self.mask_image = image
-		"""
 		filter_mask = self.filter()
+
+		self.final_filter_mask = 255 * (1-filter_mask)
+		#cv2.imshow('inverse of filter', self.final_filter_mask)
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
+
 		directional_mask = self.directional()
 
-		self.final_filter_mask = 1 - (filter_mask * directional_mask)
+		self.final_filter_mask = 255 * (1-directional_mask)
+		#cv2.imshow('inverse of directional', self.final_filter_mask)
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
+
+		#self.final_filter_mask = 1 - (filter_mask * directional_mask)
+		self.final_filter_mask = (filter_mask * directional_mask)
 
 		self.final_filter_mask = 255 * self.final_filter_mask
-		cv2.imshow('this thing', self.final_filter_mask)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
+		#cv2.imshow('final mask', self.final_filter_mask)
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
 
 		return self.final_filter_mask
 
@@ -80,7 +78,7 @@ class Filter:
 				if x <= self.cutoff:
 					mask[j][i] = 1
 
-		if not self.inverse:
+		if self.inverse:
 			mask = 1 - mask
 
 		return mask
@@ -97,7 +95,7 @@ class Filter:
 				if x <= ring_max and x >= ring_min:
 					mask[j][i] = 1
 
-		if not self.inverse:
+		if self.inverse:
 			mask = 1 - mask
 
 		return mask
@@ -110,7 +108,7 @@ class Filter:
 				x = -((j-mask.shape[0]/2)**2 + (i-mask.shape[1]/2)**2)
 				mask[j][i] = np.exp(x/((2*self.cutoff)**2))
 
-		if not self.inverse:
+		if self.inverse:
 			mask = 1 - mask
 
 		return mask
@@ -127,7 +125,7 @@ class Filter:
 				x = (((d**2)-(self.cutoff**2))/(d*self.ringwidth))
 				mask[j][i] = np.exp(-(x**2))
 
-		if not self.inverse:
+		if self.inverse:
 			mask = 1 - mask
 
 		return mask
@@ -140,7 +138,7 @@ class Filter:
 				x = ((j-(mask.shape[0]/2))**2 + (i-(mask.shape[1]/2))**2)**0.5
 				mask[j][i] = 1/(1 + ((x/self.cutoff)**(2*self.order)))
 
-		if not self.inverse:
+		if self.inverse:
 			mask = 1 - mask
 
 		return mask
@@ -157,7 +155,7 @@ class Filter:
 				x = (d*self.ringwidth)/((d**2)-(self.cutoff**2))
 				mask[j][i] = 1 - (1/(1 + (x**(2*self.order))))
 
-		if not self.inverse:
+		if self.inverse:
 			mask = 1 - mask
 
 		return mask
@@ -166,11 +164,45 @@ class Filter:
 		pass
 
 	def directional(self):
-		mask = np.zeros((self.shape[0],self.shape[1]))
+		#matrix of all 1s
+		mask = 1 - np.zeros((self.shape[0],self.shape[1]))
 
-		if not self.inverse:
-			mask = 1 - mask
+		if self.thetaspan >= 180:
+			return mask
+
+		self.theta = self.theta * math.pi / 180
+		self.thetaspan = self.thetaspan * math.pi / 180
+
+		#t1, t2 is the range of theta; the whole range spans 0 - 2pi radians. if t1 = t2, it is 2pi
+		t1 = self.theta - (self.thetaspan)
+		t2 = self.theta + (self.thetaspan)
+		t1 = math.tan(t1)
+		t2 = math.tan(t2)
+
+		for j in range(mask.shape[0]):
+			cart_j = mask.shape[0]//2 - j
+			
+			if t1 == 0:
+				i_1 = 0
+			else:
+				i_1 = cart_j/t1
+
+			if t2 == 0:
+				i_2 = mask.shape[1]
+			else:
+				i_2 = cart_j/t2
+
+			i_1 = mask.shape[1]//2 + i_1
+			i_2 = mask.shape[1]//2 + i_2
+
+			for i in range(mask.shape[1]):
+				if i > i_1 and i < i_2:
+					mask[j][i] = 0
+				elif i < i_1 and i > i_2:
+					mask[j][i] = 0
+
 		return mask
 
-newFilter = Filter((500,500), "butterworth", 100, 0, 0, circle = False, ringwidth = 4, order = 10)
+#newFilter = Filter(//shape, //function, //cutoff, //theta, //theta span, //*inverse, //*circle, //*ringwidth, //*order)
+newFilter = Filter((500,500), "ideal", 100, 45, 180, ringwidth = 4, order = 10)
 newFilter.generateMask()
