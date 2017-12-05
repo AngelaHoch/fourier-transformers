61d0920803df55b0574bcb936511549cbf3b9ee3
Angela H <ahoch2@uh.edu>
2017-12-04 18:31:06 -0600

    generates line plot

--- Test.py
+++ Test.py
@@ -1,9 +1,10 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import timeit
 
 if __name__ == '__main__':
     implementations = ['opencv', 'numpy', 'naive']
     sizes = ['image_16', 'image_32', 'image_64', 'image_128', 'image_256', 'image_512']
+    #colors = ['c', 'm', 'y', 'k', 'burlywood']
 
     times = {}
     for i in implementations:
@@ -12,7 +13,14 @@ if __name__ == '__main__':
             setup = 'from TestFourierTransform import ft_{}, {}'.format(i, size)
             times[i][size] = timeit.timeit('ft_{}({})'.format(i, size), setup=setup, number=10)
 
-    print(times)
-
-    # Generate matplotlib line plot for each implementation and size
+    for i in times.keys():
+        size = []
+        time = []
+        for s in sizes:
+            size = size + [s]
+            time = time + [times[i][s]]
+        plt.plot(range(len(time)), time, label = i)
+        plt.xticks(range(len(size)), size)
     
+    plt.legend(bbox_to_anchor=(0.4, 0.75), bbox_transform=plt.gcf().transFigure)
+    plt.show()
